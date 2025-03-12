f816f87fb01d2cab84ddf2921a83fbe7594d84da
Omsber <xyzsyx@163.com>
2025-03-12 15:35:01 +0800

    refactor: 重构应用初始化逻辑
    - 移除 GlobalComponents 插件注册，如有需要可在子项目的 App 组件中处理

--- src/main.js
+++ src/main.js
@@ -2,14 +2,11 @@ import { createApp } from 'vue'
 import pinia from '@/stores'
 import router from '@/router'
 import antDesignConfig from '@/configs/antDesignConfig'
-import GlobalComponents from '@app/plugins/globalComponents'
-import App from '@app/App'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
-app.use(GlobalComponents)
 app.mount('#root')
 
 antDesignConfig(app)
