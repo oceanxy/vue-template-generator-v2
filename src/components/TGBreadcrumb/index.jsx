571febc4e7cd1629d1b2dd0671a9dadaafeb9803
Omsber <xyzsyx@163.com>
2025-03-19 10:19:03 +0800

    refactor(components): 优化面包屑组件逻辑
    - 优化处理最后两级重名情况的逻辑
    - 新增移除最后一项子级的功能，避免面包屑最后一级出现下拉菜单

--- src/components/TGBreadcrumb/index.jsx
+++ src/components/TGBreadcrumb/index.jsx
@@ -14,17 +14,21 @@ export default {
 
       // 由于 '/' 路由的第一个子路由通常配置为首页的跳转路由，当进入首页时，route.matched 会将路由为 '/' 和 其第一个子路由
       // 同时返回，所以这里需要处理一下，以防面包屑显示为“首页 / 首页”的情况或类似情况。
-      if (matchedRoutes.length === 2 && matchedRoutes[1].name === matchedRoutes[0].redirect.name) {
-        matchedRoutes.shift()
-      }
+      // if (matchedRoutes.length === 2 && matchedRoutes[1].name === matchedRoutes[0].redirect.name) {
+      //   matchedRoutes.shift()
+      // }
 
       // 处理面包屑出现最后两级重名的情况
       // 主要出现在父级菜单设置“hideChildren: true”（不在左侧菜单展示子级），同时子级路由的 path 字段为空字符串的情况
-      const pathOfLastRoute = matchedRoutes[matchedRoutes.length - 1].path
-
-      if (pathOfLastRoute.substring(pathOfLastRoute.length - 1) === '/') {
+      if (
+        matchedRoutes.at(-1).path.at(-1) === '/'||
+        matchedRoutes.at(-2).path === matchedRoutes.at(-1).path
+      ) {
         matchedRoutes.pop()
       }
+
+      // 移除最后一项的子级，避免最后一级面包屑出现下拉菜单
+      matchedRoutes.at(-1).children = []
 
       // 替换面包屑第一级的名称
       // if (matchedRoutes[0]?.path === '') {
