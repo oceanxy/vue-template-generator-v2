3e6b76ad448fc0283b12c02ef2e93f1678e12598
Omsber <xyzsyx@163.com>
2025-04-10 17:48:35 +0800

    refactor(TGLoginWithToken): 优化全局配置字段名称

--- src/components/TGLoginWithToken/index.jsx
+++ src/components/TGLoginWithToken/index.jsx
@@ -2,7 +2,7 @@ import { defineComponent, nextTick, onMounted } from 'vue'
 import { message } from 'ant-design-vue'
 import { getCookie } from '@/utils/cookie'
 import { getFirstLetterOfEachWordOfAppName } from '@/utils/utilityFunction'
-import config from '@/configs'
+import configs from '@/configs'
 import useStore from '@/composables/tgStore'
 
 const appName = getFirstLetterOfEachWordOfAppName()
@@ -13,14 +13,14 @@ export default defineComponent({
     const loginStore = useStore('/login')
 
     onMounted(async () => {
-      const searchToken = new URL(window.location.href).searchParams.get(config.tokenConfig.fieldName)
-      const localToken = localStorage.getItem(`${appName}-${config.tokenConfig.fieldName}`)
+      const searchToken = new URL(window.location.href).searchParams.get(configs.tokenConfig.fieldName)
+      const localToken = localStorage.getItem(`${appName}-${configs.tokenConfig.fieldName}`)
       // 使用局部变量代替直接操作 props.token
       let tokenFromProps = props.token || null
 
       const token = searchToken ||
         tokenFromProps ||
-        getCookie(config.tokenConfig.fieldName) ||
+        getCookie(configs.tokenConfig.fieldName) ||
         localToken
 
       // 如果 search 中存在 token，则删除之
@@ -35,7 +35,7 @@ export default defineComponent({
 
       if (token) {
         message.destroy()
-        localStorage.setItem(`${appName}-${config.tokenConfig.fieldName}`, token)
+        localStorage.setItem(`${appName}-${configs.tokenConfig.fieldName}`, token)
 
         const response = await loginStore.getUserInfo({ token })
 
@@ -47,7 +47,7 @@ export default defineComponent({
 
           await loginStore.clear()
           // 接口调用失败请了token
-          localStorage.removeItem(`${appName}-${config.tokenConfig.fieldName}`)
+          localStorage.removeItem(`${appName}-${configs.tokenConfig.fieldName}`)
           emit('errorStateChange', { status: true, error: new Error(response.message || '获取用户信息失败') })
 
           nextTick(() => {
