0911f75e73cd38c31c0d0194877c0db530a77738
Omsber <xyzsyx@163.com>
2025-04-30 16:42:03 +0800

    feat(TGColorPicker): 优化颜色选择器组件
    - 重构组件结构，使用 InputGroup 组件包裹颜色预览和清空按钮
    - 添加清空颜色功能

--- src/components/TGColorPicker/index.jsx
+++ src/components/TGColorPicker/index.jsx
@@ -1,7 +1,8 @@
 import './index.scss'
 import { Sketch } from '@ckpack/vue-color'
-import { Popover } from 'ant-design-vue'
+import { Button, Input, InputGroup, Popover } from 'ant-design-vue'
 import { ref, watch } from 'vue'
+import { ClearOutlined } from '@ant-design/icons-vue'
 
 export default {
   name: 'TGColorPicker',
@@ -24,40 +25,48 @@ export default {
     }
 
     return () => (
-      <Popover
-        vModel:open={visible.value}
-        trigger="click"
-        overlayClassName="tg-color-picker-popover"
-      >
-        {{
-          default: () => (
-            <div
-              class="tg-color-picker-preview"
-              style={{ display: 'flex', alignItems: 'center', gap: '8px' }}
-            >
-              <div
-                class="color-block"
-                style={{
-                  width: '16px',
-                  height: '16px',
-                  borderRadius: '4px',
-                  backgroundColor: colorValue.value,
-                  cursor: 'pointer',
-                  border: '1px solid #d9d9d9'
+      <InputGroup class={'tg-color-picker-preview'}>
+        <Popover
+          vModel:open={visible.value}
+          trigger="click"
+          overlayClassName="tg-color-picker-popover"
+        >
+          {{
+            default: () => (
+              <Input
+                readonly
+                placeholder="无"
+                value={colorValue.value}
+                onChange={(e) => {
+                  handleColorChange(e.target.value)
                 }}
+                prefix={
+                  <div
+                    class="color-block"
+                    style={{
+                      width: '16px',
+                      height: '16px',
+                      borderRadius: '4px',
+                      backgroundColor: colorValue.value,
+                      cursor: 'pointer',
+                      border: '1px solid #d9d9d9'
+                    }}
+                  />
+                }
+              />),
+            content: () => (
+              <Sketch
+                modelValue={colorValue.value}
+                onUpdate:modelValue={handleColorChange}
+                presetColors={[]}
               />
-              <span style={{ cursor: 'pointer' }}>{colorValue.value.toUpperCase()}</span>
-            </div>
-          ),
-          content: () => (
-            <Sketch
-              modelValue={colorValue.value}
-              onUpdate:modelValue={handleColorChange}
-              presetColors={[]}
-            />
-          )
-        }}
-      </Popover>
+            )
+          }}
+        </Popover>
+        <Button onClick={() => handleColorChange({ hex: '' })}>
+          <ClearOutlined title="清空" />
+        </Button>
+      </InputGroup>
     )
   }
 }
