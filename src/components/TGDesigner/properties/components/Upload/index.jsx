6951d699318a5b3848f851a3a2f589f2ffa16f06
Omsber <xyzsyx@163.com>
2025-06-30 16:12:45 +0800

    feat(properties): 添加 Upload 组件
    - 实现了一个名为 PropertyUpload 的新组件，用于在属性面板中上传图片
    - 组件支持手动输入图片地址和通过上传功能添加图片

---
+++ src/components/TGDesigner/properties/components/Upload/index.jsx
@@ -0,0 +1,77 @@
+import { Button, Input, Upload } from 'ant-design-vue'
+import { UploadOutlined } from '@ant-design/icons-vue'
+import { inject, ref, watch } from 'vue'
+import './index.scss'
+
+export default {
+  name: 'PropertyUpload',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  setup(props, { emit }) {
+    const PropertyUpload = inject('PropertyUpload', null)
+    const inputValue = ref(props.value)
+    const uploadFiles = ref([])
+
+    watch(() => props.value, newVal => {
+      if (newVal !== inputValue.value) {
+        inputValue.value = newVal
+        // 清空上传组件的值
+        uploadFiles.value = []
+      }
+    })
+
+    const handleInputChange = (e) => {
+      inputValue.value = e.target.value
+      uploadFiles.value = [] // 清空上传组件
+      emit('change', inputValue.value)
+    }
+
+    const handleUploadChange = (fileList) => {
+      if (fileList.length > 0 && fileList[0].status === 'done') {
+        const path = fileList[0].response?.path || ''
+        inputValue.value = path
+        emit('change', path)
+      } else {
+        uploadFiles.value = fileList
+      }
+    }
+
+    return () => (
+      <div class={'tg-designer-property-comp-upload'}>
+        <div class={'tg-designer-property-comp-upload-wrapper'}>
+          <Input
+            value={inputValue.value}
+            onChange={handleInputChange}
+            allowClear
+            maxLength={250}
+            placeholder={'请输入图片地址'}
+          />
+        </div>
+        <div class={'tg-designer-property-expend'} title={'上传图片'}>
+          {PropertyUpload ? (
+            <PropertyUpload
+              value={uploadFiles.value}
+              onPathChange={handleUploadChange}
+              buttonType={'text'}
+              placeholder={null}
+              accept={'.png,.jpg,.jpeg'}
+              limit={1}
+              prioritizeNewUploads={true}
+            />
+          ) : (
+            <Upload
+              fileList={uploadFiles.value}
+              onChange={({ fileList }) => handleUploadChange(fileList)}
+            >
+              <Button type={'text'} icon={<UploadOutlined />} />
+            </Upload>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
