07f9817977584c8f20ff5f79845db50a2b25c12d
Omsber <xyzsyx@163.com>
2025-07-07 17:42:18 +0800

    feat(layout): 重构页尾组件并支持自定义内容
    - 重新设计了页尾组件的样式和布局，增加了最小宽度限制
    - 将页尾内容分为左、中、右三个区域，支持自定义 LOGO、文本和二维码
    - 添加了自定义内边距属性
    - 优化了组件结构，提高了可扩展性和易用性

--- src/components/TGDesigner/materials/meta/layout/Footer/index.jsx
+++ src/components/TGDesigner/materials/meta/layout/Footer/index.jsx
@@ -1,7 +1,6 @@
-import getPropertyConfig, { predefinedProperties } from '@/components/TGDesigner/properties'
+import { predefinedProperties } from '@/components/TGDesigner/properties'
 import { TG_MATERIAL_CATEGORY, TG_MATERIAL_PREVIEW_TYPE } from '@/components/TGDesigner/materials'
 import { styleWithUnits } from '@/components/TGDesigner/utils/style'
-import { QRCode } from 'ant-design-vue'
 import { ref, watch } from 'vue'
 import './assets/styles/index.scss'
 
@@ -12,7 +11,7 @@ import './assets/styles/index.scss'
 export default {
   type: 'tg-layout-footer',
   category: TG_MATERIAL_CATEGORY.LAYOUT,
-  name: '页尾',
+  name: '页尾容器',
   preview: props => {
     if (props.previewType !== TG_MATERIAL_PREVIEW_TYPE.MATERIAL) {
       return <Footer {...props} />
@@ -21,20 +20,22 @@ export default {
     return <IconFont type="icon-designer-footer" />
   },
   defaultProps: {
-    contentWidth: '90%'
+    contentWidth: '100%',
+    contentPadding: 0
   },
   style: {
     width: '100%',
     height: '',
-    padding: '0 30',
+    padding: 5,
     margin: 0,
-    backgroundColor: '#0050b3',
+    backgroundColor: '',
     backgroundImage: '',
     backgroundSize: '',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat'
   },
   class: '',
+  children: [],
   propConfigForm: propertyValues => [
     {
       label: '尺寸',
@@ -51,8 +52,13 @@ export default {
           title: 'Footer内展示内容区域容器的宽度（width）',
           prop: 'contentWidth',
           props: {
-            placeholder: '90%'
+            placeholder: '100%'
           }
+        }),
+        predefinedProperties.padding({
+          label: '内容区域内边距',
+          title: 'Footer内展示内容区域的容器内边距值（padding）',
+          prop: 'contentPadding'
         }),
         predefinedProperties.padding(),
         predefinedProperties.margin()
@@ -67,65 +73,75 @@ export default {
 
 export const Footer = {
   name: 'Footer',
-  props: ['contentWidth', 'style', 'previewType', 'children'],
+  props: ['contentWidth', 'contentPadding', 'style', 'previewType', 'children'],
   setup(props, { attrs }) {
     const style = ref({})
+    const isDesignPatterns = props.previewType === TG_MATERIAL_PREVIEW_TYPE.CANVAS ||
+      props.previewType === TG_MATERIAL_PREVIEW_TYPE.MATERIAL_PREVIEW
 
     watch(() => props.style, val => {
       style.value = styleWithUnits(val)
+
+      // 当未设置背景色时，使用一个默认的内置背景色
+      if (!style.value.backgroundColor && !style.value.backgroundImage) {
+        style.value.backgroundColor = '#101e75'
+      }
     }, { immediate: true })
 
-    return () => {
-      if (attrs.device === 'h5') {
-        return (
-          <div
-            class="tg-designer-layout-footer"
-            style={{
-              ...style.value,
-              'padding': '2rem 1rem'
-            }}
-          >
-            <div class="tg-designer-layout-footer-content">
-              <div>
-                <div class="tg-designer-layout-footer-content-title">
-                  <img
-                    src="https://oss.bj-dx-dzqywjd-hlw-icp.inspurcloudoss.com:6063/tygzt01/upload/cServ/c-serv/5735606129182378245.png"
-                    alt=""
-                  />
-                </div>
-                <div>中国科学技术协会版权所有 京ICP 备 10216604号-4 海淀分局备案 1101084647</div>
-                <div>中国科学技术协会主办</div>
-              </div>
-            </div>
-          </div>
-        )
-      }
-
-      return (
-        <div
-          class="tg-designer-layout-footer"
-          style={style.value}
-        >
-          <div
-            class="tg-designer-layout-footer-content"
-            style={styleWithUnits({ width: props.contentWidth || '100%' })}
-          >
-            <div>
-              <div class="tg-designer-layout-footer-content-title">
-                <img
-                  src="https://oss.bj-dx-dzqywjd-hlw-icp.inspurcloudoss.com:6063/tygzt01/upload/cServ/c-serv/5735606129182378245.png"
-                  alt=""
-                />
-              </div>
-              <div>中国科学技术协会版权所有 京ICP 备 10216604号-4 海淀分局备案 1101084647</div>
-              <div>中国科学技术协会主办</div>
-            </div>
-            <div>
-              <QRCode value={''} color={'#ffffff'} />
-            </div>
-          </div>
-        </div>
-      )
+    const findChild = cellPosition => {
+      return props.children?.find(child => child.props?.['data-cell-position'] === cellPosition)
     }
+
+    return () => (
+      <div
+        class="tg-designer-layout-footer"
+        style={style.value}
+      >
+        <div
+          class="tg-designer-layout-footer-content"
+          style={styleWithUnits({
+            width: props.contentWidth || '100%',
+            padding: props.contentPadding || 0
+          })}
+        >
+          <div class={'tg-designer-layout-footer-content-left'}>
+            <div
+              data-cell-position="logo"
+              class={{
+                'tg-designer-layout-container': true,
+                'tg-designer-layout-logo': true,
+                'is-design-patterns': isDesignPatterns
+              }}
+            >
+              {findChild('logo')}
+            </div>
+            <div
+              data-cell-position="text"
+              class={{
+                'tg-designer-layout-container': true,
+                'tg-designer-layout-text': true,
+                'is-design-patterns': isDesignPatterns
+              }}
+            >
+              {findChild('text')}
+            </div>
+          </div>
+          {
+            attrs.device !== 'h5' && (
+              <div
+                data-cell-position="qrcode"
+                class={{
+                  'tg-designer-layout-container': true,
+                  'tg-designer-layout-qrcode': true,
+                  'is-design-patterns': isDesignPatterns
+                }}
+              >
+                {findChild('qrcode')}
+              </div>
+            )
+          }
+        </div>
+      </div>
+    )
   }
 }
