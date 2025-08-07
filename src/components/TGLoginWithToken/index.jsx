fccfb50029c2c2ebdf1352cbf19a31d547fdef4f
Omsber <xyzsyx@163.com>
2025-08-07 17:43:16 +0800

    refactor(TGLoginWithToken): 从vue2组件迁移

--- src/components/TGLoginWithToken/index.jsx
+++ src/components/TGLoginWithToken/index.jsx
@@ -1,73 +1,57 @@
-import { defineComponent, nextTick, onMounted } from 'vue'
+/**
+ * 通过第三方token登录
+ */
+
 import { message } from 'ant-design-vue'
 import { getCookie } from '@/utils/cookie'
 import { getFirstLetterOfEachWordOfAppName } from '@/utils/utilityFunction'
-import configs from '@/configs'
+import { nextTick, onBeforeMount } from 'vue'
+import { useRoute } from 'vue-router'
+import config from '@/config'
 import useStore from '@/composables/tgStore'
 
 const appName = getFirstLetterOfEachWordOfAppName()
 
-export default defineComponent({
+export default {
   name: 'TGLoginWithToken',
-  props: {
-    /**
-     * 是否优先获取地址栏中的 token ，默认true
-     * @type {boolean}
-     */
-    isPriorityHrefToken: {
-      type: Boolean,
-      default: true
-    }
-  },
-  setup(props, { emit }) {
+  async setup(props, { emit }) {
     const loginStore = useStore('/login')
+    const route = useRoute()
+    const searchToken = new URL(window.location.href).searchParams.get(config.tokenConfig.fieldName)
+    const localToken = localStorage.getItem(`${appName}-${config.tokenConfig.fieldName}`)
 
-    onMounted(async () => {
-      let searchToken = null
-      // 是否优先获取地址栏token
-      if (props.isPriorityHrefToken) {
-        searchToken = new URL(window.location.href).searchParams.get(configs.tokenConfig.fieldName)
-      }
+    const token = searchToken ||
+      route.query[config.tokenConfig.fieldName] ||
+      getCookie(config.tokenConfig.fieldName) ||
+      localToken
 
-      const localToken = localStorage.getItem(`${appName}-${configs.tokenConfig.fieldName}`)
-      // 使用局部变量代替直接操作 props.token
-      let tokenFromProps = props.token || null
-
-      const token = searchToken ||
-        tokenFromProps ||
-        getCookie(configs.tokenConfig.fieldName) ||
-        localToken
-
+    onBeforeMount(async () => {
       // 如果 search 中存在 token，则删除之
       if (searchToken) {
         window.history.replaceState(null, null, window.location.pathname)
       }
 
-      // 如果 hash 中存在 token，将其置为 null 而非直接删除
-      if (tokenFromProps) {
-        tokenFromProps = null
+      // 如果 hash 中存在 token，则删除之
+      if (route.query.token) {
+        delete route.query.token
       }
 
       if (token) {
         message.destroy()
-        localStorage.setItem(`${appName}-${configs.tokenConfig.fieldName}`, token)
 
         const response = await loginStore.getUserInfo({ token })
 
         if (response.status) {
-          emit('errorStateChange', { status: false, error: null })
           await loginStore.jumpAfterLogin()
+          emit('errorStateChange', { status: false, error: null })
         } else {
           message.error(response.message || '获取用户信息失败', 0)
 
           await loginStore.clear()
-          // 接口调用失败请了token
-          localStorage.removeItem(`${appName}-${configs.tokenConfig.fieldName}`)
           emit('errorStateChange', { status: true, error: new Error(response.message || '获取用户信息失败') })
 
-          nextTick(() => {
-            jumpToThirdPartyLogin()
-          })
+          await nextTick()
+          jumpToThirdPartyLogin()
         }
       } else {
         message.error('未检测到登录令牌或登录令牌已失效', 0)
@@ -79,7 +63,7 @@ export default defineComponent({
       }
     })
 
-    const jumpToThirdPartyLogin = () => {
+    function jumpToThirdPartyLogin() {
       if (process.env.VUE_APP_LOGIN_ADDRESS) {
         let url
 
@@ -97,4 +81,4 @@ export default defineComponent({
 
     return () => null
   }
-})
+}
