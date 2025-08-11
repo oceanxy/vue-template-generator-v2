5982d6ba29085e92c1ded58bce998bea40d96d66
Omsber <xyzsyx@163.com>
2025-08-11 17:47:02 +0800

    refactor(demo): 简化 App 组件结构，该功能已经集成到useThemeApp中

--- src/apps/demo/App.jsx
+++ src/apps/demo/App.jsx
@@ -1,29 +1,15 @@
-import 'ant-design-vue/dist/reset.css'
-import '@/assets/styles/app.scss'
-import { RouterView } from 'vue-router'
 import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
 import useThemeApp from '@/composables/tgThemeApp'
-import globalComponents from '@app/plugins/globalComponents'
-import { onBeforeMount } from 'vue'
-import { app } from '@/main'
+import 'ant-design-vue/dist/reset.css'
+import '@/assets/styles/app.scss'
 
 dayjs.locale('zh-cn')
 
 export default {
   setup() {
-    onBeforeMount(() => {
-      app.use(globalComponents)
-    })
-
     const { ThemeApp } = useThemeApp()
 
-    return () => (
-      <ThemeApp>
-        <RouterView />
-        {/* 定义弹窗的容器，可根据项目需求自行定义到其他地方 */}
-        <div id="tg-global-modals"></div>
-      </ThemeApp>
-    )
+    return () => <ThemeApp />
   }
 }
