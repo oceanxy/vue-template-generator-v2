b18eaacbe48fc98f18a732abce217e412bb2e8c3
Omsber <xyzsyx@163.com>
2026-01-25 17:26:33 +0800

    refactor(build): 重构构建配置以支持多应用HTML模板
    - 在scripts/index.js中引入HtmlWebpackPlugin并按应用动态配置模板
    - 更新CopyWebpackPlugin配置支持多应用静态文件复制
    - 调整webpack.base.conf.js移除全局HTML模板配置

--- scripts/index.js
+++ scripts/index.js
@@ -8,13 +8,15 @@ const AppNameInjectionPlugin = require('./plugins/AppNameInjectionPlugin')
 const MediaQueryConfigPlugin = require('./plugins/MediaQueryConfigPlugin')
 const { merge } = require('lodash')
 const chalk = require('chalk')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const dotenv = require('dotenv')
 const dotenvExpand = require('dotenv-expand')
 const { exec } = require('child_process')
 
-const appNames = readdirSync(resolve(join(__dirname, `../src/apps`)))
-  .filter(file => statSync(resolve(join(__dirname, '../src/apps', file))).isDirectory())
+const appNames = readdirSync(resolve(join(__dirname, `../src/apps`))).filter(file =>
+  statSync(resolve(join(__dirname, '../src/apps', file))).isDirectory()
+)
 
 let appName = null
 const { NODE_ENV, BASE_ENV } = process.env
@@ -25,7 +27,10 @@ if (args.app) {
     let abbreviation = null
 
     if (_appName.includes('-')) {
-      abbreviation = _appName.split('-').map(item => item.charAt(0).toUpperCase()).join('')
+      abbreviation = _appName
+        .split('-')
+        .map(item => item.charAt(0).toUpperCase())
+        .join('')
     }
 
     if ([abbreviation, _appName].includes(args.app)) {
@@ -47,20 +52,22 @@ if (appName) {
   const options = appNames.map(item => ({ name: item, value: item }))
 
   // 使用 inquirer 创建菜单
-  inquirer.prompt([
-    {
-      type: 'list',
-      name: 'choice',
-      message: `请选择执行项目：`,
-      choices: options
-    }
-  ]).then(answers => {
-    if (args.cleanAppCache) {
-      cleanAppCache(answers.choice)
-    } else {
-      getConfig(answers.choice)
-    }
-  })
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'choice',
+        message: `请选择执行项目：`,
+        choices: options
+      }
+    ])
+    .then(answers => {
+      if (args.cleanAppCache) {
+        cleanAppCache(answers.choice)
+      } else {
+        getConfig(answers.choice)
+      }
+    })
 }
 
 function cleanAppCache(appName) {
@@ -126,39 +133,70 @@ function getConfig(appName) {
   })
 
   // 新增 IgnorePlugin，过滤掉`/apps`下其他子项目的文件
-  config.plugins.push(new webpack.IgnorePlugin({
-    checkResource(resource, context) {
-      if (
-        resource.includes('@app') &&
-        ignoreApp.some(app => context.match(new RegExp(`src[/\\\\]apps[/\\\\]${app}`)))
-      ) {
-        return true
-      }
+  config.plugins.push(
+    new webpack.IgnorePlugin({
+      checkResource(resource, context) {
+        if (
+          resource.includes('@app') &&
+          ignoreApp.some(app => context.match(new RegExp(`src[/\\\\]apps[/\\\\]${app}`)))
+        ) {
+          return true
+        }
 
-      if (ignoreApp.some(app => resource.match(new RegExp(`[/\\\\]${app}[/\\\\]`)))) {
-        return true
-      }
+        if (ignoreApp.some(app => resource.match(new RegExp(`[/\\\\]${app}[/\\\\]`)))) {
+          return true
+        }
 
-      // 排除子项目的 .md 和 .ico 资源
-      return /\.(md|ico)$/.test(resource)
-    }
-  }))
+        // 排除子项目的 .md 和 .ico 资源
+        return /\.(md|ico)$/.test(resource)
+      }
+    })
+  )
 
   // 调用注入 appName 的插件
   config.plugins.unshift(new AppNameInjectionPlugin({ appName }))
   // 调用媒体查询处理插件
   config.plugins.unshift(new MediaQueryConfigPlugin({ appName }))
 
+  try {
+    config.plugins.push(
+      new HtmlWebpackPlugin({
+        template: resolve(join(__dirname, `../src/apps/${appName}/public/index.html`)),
+        inject: true
+      })
+    )
+  } catch (error) {
+    config.plugins.push(
+      new HtmlWebpackPlugin({
+        template: resolve(join(__dirname, `../public/index.html`)),
+        inject: true
+      })
+    )
+  }
+
   // 复制 public 内静态文件
-  config.plugins.push(new CopyWebpackPlugin({
-    patterns: [
-      {
-        force: true,
-        from: resolve(join(__dirname, `../src/apps/${appName}/public`)),
-        to: resolve(join(__dirname, '../dist', appName))
-      }
-    ]
-  }))
+  config.plugins.push(
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          force: true,
+          from: resolve(join(__dirname, `../public`)), // 复制public下文件
+          to: resolve(join(__dirname, '../dist', appName)), // 复制到dist目录下当前项目目录中
+          filter: source => {
+            return !source.includes('index.html') // 忽略index.html
+          }
+        },
+        {
+          force: true,
+          from: resolve(join(__dirname, `../src/apps/${appName}/public`)), // 复制当前项目public下文件
+          to: resolve(join(__dirname, '../dist', appName)), // 复制到dist目录下当前项目目录中
+          filter: source => {
+            return !source.includes('index.html') // 忽略index.html
+          }
+        }
+      ]
+    })
+  )
 
   const compiler = webpack(config)
 
@@ -170,7 +208,7 @@ function getConfig(appName) {
 
       console.log(
         chalk.hex('#1fb0ff')(`<i> [${appName}] `) +
-        chalk.gray(`检测到 Dev Server 配置文件（src/apps/${appName}/configs/devServer.js），已成功加载。`)
+          chalk.gray(`检测到 Dev Server 配置文件（src/apps/${appName}/configs/devServer.js），已成功加载。`)
       )
 
       const runServer = async () => {
--- scripts/webpack.base.conf.js
+++ scripts/webpack.base.conf.js
@@ -1,13 +1,12 @@
 const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { DefinePlugin, ProvidePlugin } = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const GlobalVariableInjectionPlugin = require('./plugins/GlobalVariableInjectionPlugin')
-const CopyPlugin = require('copy-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development' // 是否是开发模式
 const TG_APP_NAME = process.env.TG_APP_NAME
+const TG_APP_PUBLIC_PATH = process.env.TG_APP_PUBLIC_PATH
 
 /**
  * 获取符合前缀规则的环境变量对象
@@ -15,7 +14,7 @@ const TG_APP_NAME = process.env.TG_APP_NAME
 const resolveClientEnv = () => {
   const env = {}
 
-  Object.keys(process.env).forEach((key) => {
+  Object.keys(process.env).forEach(key => {
     if (/^TG_APP_/.test(key) || key === 'NODE_ENV' || key === 'BASE_ENV') {
       env[key] = JSON.stringify(process.env[key])
     }
@@ -31,7 +30,7 @@ module.exports = {
     filename: 'static/js/[name].[chunkhash:8].js', // 每个输出js的名称
     path: path.join(__dirname, '../dist', TG_APP_NAME), // 打包结果输出路径
     clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
-    publicPath: '/' // 打包后文件的公共前缀路径
+    publicPath: TG_APP_PUBLIC_PATH // 打包后文件的公共前缀路径
   },
   resolve: {
     extensions: ['.vue', '.js', '.jsx', '.json'],
@@ -39,7 +38,7 @@ module.exports = {
     alias: {
       '@': path.join(__dirname, '../src'),
       '@app': path.join(__dirname, `../src/apps/${TG_APP_NAME}`),
-      'process': 'process/browser'
+      process: 'process/browser'
     },
     // 如果用的是 pnpm 就暂时不要配置这个，会有幽灵依赖的问题，访问不到很多模块。
     modules: [path.resolve(__dirname, '../node_modules')] // 查找第三方模块只在本项目的node_modules中查找
@@ -48,10 +47,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/, // 匹配 css 文件
-        include: [
-          path.resolve(__dirname, '../node_modules/ant-design-vue/dist'),
-          path.resolve(__dirname, '../src')
-        ],
+        include: [path.resolve(__dirname, '../node_modules/ant-design-vue/dist'), path.resolve(__dirname, '../src')],
         use: [
           // 开发环境使用style-loader，打包模式抽离css
           isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
@@ -179,10 +175,6 @@ module.exports = {
   },
   plugins: [
     new VueLoaderPlugin(), // vue-loader插件
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html'), // 模板取定义root节点的模板
-      inject: true // 自动注入静态资源
-    }),
     new DefinePlugin({
       __VUE_OPTIONS_API__: 'true',
       __VUE_PROD_DEVTOOLS__: 'false',
@@ -191,18 +183,6 @@ module.exports = {
     }),
     new ProvidePlugin({
       process: 'process/browser'
-    }),
-    // 复制文件插件
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, '../public'), // 复制public下文件
-          to: path.resolve(__dirname, '../dist'), // 复制到dist目录中
-          filter: source => {
-            return !source.includes('index.html') // 忽略index.html
-          }
-        }
-      ]
     }),
     // 注意：根据 webpack 插件调用机制，此内部插件必须在 DefinePlugin 和 ProvidePlugin 插件之后调用
     new GlobalVariableInjectionPlugin()
